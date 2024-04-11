1736322ae136f97f2b75fb4a7ceb79447dc678c8
gwebbmoney <gwebb@red-aspen.com>
2024-04-10 23:24:35 -0600

    Made changes to column names

--- basic_information.py
+++ basic_information.py
@@ -5,13 +5,21 @@ import yfinance as yf
 
 #Ticker symbol for Oracle
 orcl = yf.Ticker("ORCL")
+msft = yf.Ticker("MSFT")
+googl = yf.Ticker("GOOGL")
+adbe = yf.Ticker('ADBE')
+crm = yf.Ticker('CRM')
+csco = yf.Ticker('CSCO')
+intu = yf.Ticker('INTU')
+sap = yf.Ticker('SAP')
 
+#Income Statements
 orcl_yoy_income_stmt = yf.Ticker.get_income_stmt(orcl)
 
 df1 = pd.DataFrame(orcl_yoy_income_stmt)
 
 #Desired order for income statement
-desired_index_order =[
+orcl_desired_index_order =[
     'TotalRevenue', 'OperatingRevenue', 'CostOfRevenue', 'GrossProfit', 'OperatingExpense', 'SellingGeneralAndAdministration',
     'GeneralAndAdministrativeExpense', 'SalariesAndWages', 'OtherGandA', 'SellingandMarketingExpense', 'ResearchAndDevelopment',
     'DepreciationAmortizationDepletionIncomeStatement', 'DepreciationAndAmortizationInIncomeStatement', 'Amortization', 'AmortizationOfIntangiblesIncomeStatement',
@@ -25,19 +33,19 @@ desired_index_order =[
     'TotalUnusualItems', 'NormalizedEBITDA', 'TaxRateForCalcs', 'TaxEffectOfUnusualItems'
 ]
 
-ordered_income_statement = df1.reindex(desired_index_order)
+ordered_income_statement = df1.reindex(orcl_desired_index_order)
 
 st.dataframe(
     ordered_income_statement,
     column_config={
-        "": st.column_config.Column("Breakdown", width="1400"),
+        "": st.column_config.Column("Breakdown", width="1800"),
         "2023-05-31 00:00:00": "2023",
         "2022-05-31 00:00:00": "2022",
         "2021-05-31 00:00:00": "2021",
         "2020-05-31 00:00:00": "2020"
     },
-    width=1200,
-    height=1200
+    width=1600,
+    height=700
 )
 
 
